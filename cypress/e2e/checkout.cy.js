f0df66044a589625fc2d231ca48c56300569e246
Mysterion147 <joaopedromaciel0@gmail.com>
2026-03-10 01:56:18 -0300

    checkout flow test suite

---
+++ cypress/e2e/checkout.cy.js
@@ -0,0 +1,112 @@
+describe("Checkout Flow", () => {
+  beforeEach(function () {
+    cy.fixture("users").as("userData");
+    cy.visit("/");
+
+    cy.get("@userData").then((data) => {
+      cy.login(data.standard.user, data.standard.pass);
+    });
+  });
+
+  it("Should complete a purchase with dynamic name, price and tax validation", function () {
+    // saving both name and price of the first item
+    cy.get(".inventory_item")
+      .first()
+      .within(() => {
+        cy.get(".inventory_item_name").then(($name) => {
+          cy.wrap($name.text()).as("selectedItemName");
+        });
+
+        cy.get(".inventory_item_price").then(($price) => {
+          const priceValue = parseFloat($price.text().replace("$", ""));
+          cy.wrap(priceValue).as("selectedItemPrice");
+        });
+
+        cy.get("button").click();
+      });
+
+    // going to checkout info
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="checkout"]').click();
+
+    // filling form
+    cy.get('[data-test="firstName"]').type("John");
+    cy.get('[data-test="lastName"]').type("Doe");
+    cy.get('[data-test="postalCode"]').type("147147");
+    cy.get('[data-test="continue"]').click();
+
+    // assertion -> ensuring data integrity after changing pages
+    cy.get("@selectedItemName").then((name) => {
+      cy.get(".inventory_item_name").should("have.text", name);
+    });
+
+    cy.get("@selectedItemPrice").then((originalPrice) => {
+      // assertion -> checking order total
+      cy.get(".summary_subtotal_label").should("contain", originalPrice);
+
+      // calcullating tax (seems to be 8%) and total
+      cy.get(".summary_tax_label").then(($taxEl) => {
+        const taxValue = parseFloat($taxEl.text().replace("Tax: $", ""));
+        const expectedTotal = (originalPrice + taxValue).toFixed(2);
+
+        // assertion -> checking if the UI value matches the expected
+        cy.get(".summary_total_label").should("contain", expectedTotal);
+      });
+    });
+
+    // final assertion -> check if we got to the ending page
+    cy.get('[data-test="finish"]').click();
+    cy.get(".complete-header").should("have.text", "Thank you for your order!");
+  });
+
+  it("Should show an error message when the checkout form is incomplete (no Last Name)", function () {
+    // populating cart and checking out
+    cy.get(".inventory_item").first().find("button").click();
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="checkout"]').click();
+
+    // leaving "Last Name" empty
+    cy.get('[data-test="firstName"]').type("John");
+    cy.get('[data-test="postalCode"]').type("147147");
+    cy.get('[data-test="continue"]').click();
+
+    // assertions -> checking if the error is visible and has the correct text
+    cy.get('[data-test="error"]')
+      .should("be.visible")
+      .and("contain.text", "Error: Last Name is required");
+  });
+
+  it("Should show an error message when the checkout form is incomplete (no First Name)", function () {
+    // populating cart and checking out
+    cy.get(".inventory_item").first().find("button").click();
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="checkout"]').click();
+
+    // leaving "First Name" empty
+    cy.get('[data-test="lastName"]').type("Doe");
+    cy.get('[data-test="postalCode"]').type("147147");
+    cy.get('[data-test="continue"]').click();
+
+    // assertions -> checking if the error is visible and has the correct text
+    cy.get('[data-test="error"]')
+      .should("be.visible")
+      .and("contain.text", "Error: First Name is required");
+  });
+
+  it("Should show an error message when the checkout form is incomplete (no Zip/Postal Code)", function () {
+    // populating cart and checking out
+    cy.get(".inventory_item").first().find("button").click();
+    cy.get('[data-test="shopping-cart-link"]').click();
+    cy.get('[data-test="checkout"]').click();
+
+    // leaving "Zip/Postal Code" empty
+    cy.get('[data-test="firstName"]').type("John");
+    cy.get('[data-test="lastName"]').type("Doe");
+    cy.get('[data-test="continue"]').click();
+
+    // assertions -> checking if the error is visible and has the correct text
+    cy.get('[data-test="error"]')
+      .should("be.visible")
+      .and("contain.text", "Error: Postal Code is required");
+  });
+});
